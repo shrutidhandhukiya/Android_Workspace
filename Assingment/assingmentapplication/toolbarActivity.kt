0c12db3b5472327c1a2066b6e5c1d90b362cc7c5
shrutidhandhukiya <123073454+shrutidhandhukiya@users.noreply.github.com>
2023-04-29 11:19:00 +0530

    Add files via upload

---
+++ Assingment/assingmentapplication/toolbarActivity.kt
@@ -0,0 +1,70 @@
+package com.example.assingmentapplication
+
+import android.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
+import android.widget.Toast
+import com.example.assingmentapplication.databinding.ActivityToolbarBinding
+
+class toolbarActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityToolbarBinding
+    lateinit var list: MutableList<String>
+     var arr = arrayOf("Rajkot ", "Surat ","Amreli ", "Dhari")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityToolbarBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        list = ArrayList()
+        //  setSupportActionBar(binding.toolbar)
+
+        list.add("Rajkot")
+        list.add("shruti")
+        list.add("Java")
+        list.add("Rajkot")
+        list.add("Rajkot")
+
+        var adapter =
+            ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, list)
+        binding.listItem.adapter = adapter
+
+
+         var adapter1 = ArrayAdapter(applicationContext,android.R.layout.simple_spinner_dropdown_item,arr)
+          binding.spinner.adapter = adapter1
+
+        binding.spinner.onItemSelectedListener = this
+
+
+     binding.serch.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+         override fun onQueryTextSubmit(p0: String?): Boolean {
+
+             return false
+         }
+
+         override fun onQueryTextChange(p0: String?): Boolean {
+             adapter.filter.filter(p0)
+             return true
+         }
+     })
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        Toast.makeText(applicationContext,""+arr[p2],Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        
+    }
+
+}
